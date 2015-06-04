2d2649701f8d4bf7c28184c75f28629f44dc9090
Cole Gleason <colewashere@gmail.com>
2015-06-04 11:06:32 -0700

    personal update

--- personal/cole.el
+++ personal/cole.el
@@ -37,4 +37,38 @@
               (define-key js2-mode-map "\C-cj" 'js-doc-insert-function-doc)
               (define-key js2-mode-map "@" 'js-doc-insert-tag)))
 
+;; TODO lists, ha
+(setq org-agenda-files (list "~/Dropbox/Documents/todo.org"))
+
+(setq org-default-notes-file (concat org-directory "/notes.org"))
+(define-key global-map "\C-cc" 'org-capture)
+
+;; unbind 'uu' from undo-tree mode
+(key-chord-define-global "uu" nil)
+
+;; go-autocomplete with gocode
+(require 'auto-complete)
+(require 'go-autocomplete)
+(require 'auto-complete-config)
+(add-hook 'go-mode-hook 'auto-complete-mode)
+;; twitter
+(setq twittering-use-master-password t)
+(global-set-key (kbd "C-c C-t") 'twittering-update-status-interactive)
+
+(setenv "PATH" "/usr/local/heroku/bin:/usr/local/opt/ruby/bin:/opt/local/bin:/usr/local/bin:/Library/Frameworks/Python.framework/Versions/2.7/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/X11/bin:/usr/texbin/://usr/local/share/npm/bin:/Users/cole/code/android-sdk/platform-tools:~/code/go/bin")
+(setenv "GOPATH" "/Users/cole/.gvm/pkgsets/go1.3/global")
+(setq exec-path (cons "/Users/cole/.gvm/pkgsets/go1.3/global/bin" exec-path))
+(add-to-list 'exec-path "/Users/cole/.gvm/pkgsets/go1.3/global/bin")
+
+(add-hook `before-save-hook 'gofmt-before-save)
+
+(global-auto-revert-mode 0)
+
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
+    ad-do-it))
+
 ;;; cole.el ends here
